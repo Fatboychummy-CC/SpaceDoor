317a65dda6ad46eacb984695bd23ab5dcccb94d5
Fatboychummy <39929480+fatboychummy@users.noreply.github.com>
2020-08-06 01:00:17 -0700

    Add Lockdown

--- server.lua
+++ server.lua
@@ -29,21 +29,100 @@ local expect = require("cc.expect").expect
 local tClearances = {}
 local sSaveFile = ".records"
 local mx, my = term.getSize()
-local printWindow = window.create(term.current(), 1, 1, mx, my - 1)
-local userWindow  = window.create(term.current(), 1, my, mx, 1)
+local bLockDown = false
+local nLockdownChannel = 350
+local minLockdownLevel = 999
+local printWindow   = window.create(term.current(), 1, 2, mx, my - 8)
+local commandWindow = window.create(term.current(), 1, my - 6, mx, 1)
+local resultWindow  = window.create(term.current(), 1, my - 4, mx, 3)
+local userWindow    = window.create(term.current(), 1, my, mx, 1)
+
+local function Line(window, length, color)
+  window.setBackgroundColor(color)
+  window.write(string.rep(' ', length))
+end
+
+local function redrawScreen()
+  term.setBackgroundColor(colors.gray)
+  term.clear()
+  term.setCursorPos(2, 1)
+  term.write("Log Output")
+  printWindow.setBackgroundColor(colors.black)
+  printWindow.redraw()
+  term.setCursorPos(1, my - 7)
+  Line(term, mx, colors.gray)
+  term.setCursorPos(2, my - 7)
+  term.write("Last Command")
+  commandWindow.setBackgroundColor(colors.black)
+  commandWindow.redraw()
+  term.setCursorPos(2, my - 5)
+  term.write("Last Command Results")
+  userWindow.setBackgroundColor(colors.black)
+  userWindow.redraw()
+  term.setCursorPos(2, my - 1)
+  term.write("User Input")
+  resultWindow.setBackgroundColor(colors.black)
+  resultWindow.redraw()
+  term.setBackgroundColor(colors.black)
+end
 
 local oprint = print
 local function print(...)
   local lx, ly = term.getCursorPos()
   local old = term.redirect(printWindow)
-
+  printWindow.setBackgroundColor(colors.black)
   oprint(...)
 
   term.redirect(old)
+  redrawScreen()
   term.setCursorPos(lx, ly)
 end
+term.setBackgroundColor(colors.gray)
+term.clear()
+printWindow.setBackgroundColor(colors.black)
+printWindow.clear()
+commandWindow.setBackgroundColor(colors.black)
+commandWindow.clear()
+userWindow.setBackgroundColor(colors.black)
+userWindow.clear()
+resultWindow.setBackgroundColor(colors.black)
+resultWindow.clear()
 
 -- [[ Functions ]] --
+
+
+local function CommandResult(sCommand, tResult)
+  expect(1, sCommand, "string")
+  expect(2, tResult, "table")
+
+  commandWindow.clear()
+  commandWindow.setCursorPos(1, 1)
+  commandWindow.write(sCommand)
+
+  resultWindow.clear()
+  resultWindow.setCursorPos(1, 1)
+  resultWindow.write(string.format("Status: %s", tResult.status))
+
+  resultWindow.setCursorPos(1, 2)
+  if tResult.status == "error" then
+    resultWindow.write(tResult.error)
+  elseif tResult.status == "ok" then
+    resultWindow.write(tResult.output)
+  elseif tResult.status == "no-subcommand" then
+    resultWindow.write(string.format("No subcommand: %s", tResult.subcommand))
+  elseif tResult.status == "console" then
+    resultWindow.write("Check console.")
+  end
+end
+
+local function CommandPrint(tLines)
+  print("# COMMAND RESULT #")
+  for i = 1, #tLines do
+    print(tLines[i])
+  end
+  print("##################")
+end
+
 local function DefineSettings()
   local function DefineDefault(sSetting, val)
     settings.define(sSetting, {type = type(val), default = val})
@@ -78,7 +157,6 @@ end
 
 local function Action(command)
   expect(1, command, "string")
-  print("COMMAND:", command)
 
   local chunks = {}
   for substring in command:gmatch("%S+") do
@@ -86,60 +164,109 @@ local function Action(command)
   end
 
   local actions = {}
+  function actions.clear()
+    printWindow.clear()
+    printWindow.setCursorPos(1, 1)
+    CommandResult(command, {status = "ok", output = "Console cleared."})
+  end
+  function actions.lockdown(active)
+    if active == "activate" or active == "true" or active == "yes" or active == "y" or active == "active" then
+      -- activate lockdown
+      if bLockDown then
+        CommandResult(command, {status = "error", error = "Lockdown already in effect!"})
+        return
+      end
+      bLockDown = true
+      os.queueEvent("lockdown")
+      CommandResult(command, {status = "ok", output = "Lockdown activated!"})
+    elseif active == "deactivate" or active == "false" or active == "no" or active == "n" or active == "inactive" then
+      -- deactivate lockdown
+      if not bLockDown then
+        CommandResult(command, {status = "error", error = "No lockdown in effect!"})
+        return
+      end
+      bLockDown = false
+      CommandResult(command, {status = "ok", output = "Lockdown deactivated!"})
+    elseif active == "status" then
+      -- show lockdown status
+      CommandResult(command, {status = "ok", output = string.format("Lockdown status: %s", bLockDown and "ACTIVE" or "INACTIVE")})
+    else
+      CommandResult(command, {status = "no-subcommand", subcommand = active})
+    end
+  end
   function actions.record(_type, uuid, level)
     if _type == "add" then
       if tClearances[uuid] then
-        print("  That uuid already exists! Use 'record update <uuid>' instead.")
+        CommandResult(command, {status = "error", error = "UUID already exists. Use 'record update <uuid>'."})
         return
       elseif not tonumber(level) then
-        print("  Usage: record add <uuid:string> <level:number>")
+        CommandResult(command, {status = "error", error = "Expected number as argument #4."})
         return
       end
       UpdateRecord(uuid, tonumber(level))
-      print("  Added uuid", uuid, "with level", level)
+      CommandResult(command, {status = "ok", output = string.format("Added uuid %s with level %d", uuid, tonumber(level))})
     elseif _type == "update" then
       if not tClearances[uuid] then
-        print("  That uuid doesn't exist! Use 'record add <uuid>' instead.")
+        CommandResult(command, {status = "error", error = "UUID does not exist! Use 'record add <uuid>'."})
         return
       elseif not tonumber(level) then
-        print("  Usage: record update <uuid:string> <level:number>")
+        CommandResult(command, {status = "error", error = "Expected number as argument #4."})
         return
       end
       local old = tClearances[uuid]
       UpdateRecord(uuid, tonumber(level))
-      print("  Updated uuid", uuid, " (", old, "-->", level, ")")
+      CommandResult(command, {status = "ok", output = string.format("Updated uuid %s (%d --> %d)", uuid, old, level)})
     elseif _type == "remove" then
       if not tClearances[uuid] then
-        print("  That uuid doesn't exist!")
+        CommandResult(command, {status = "error", error = "UUID does not exist!"})
+        return
       end
       UpdateRecord(uuid, nil)
-      print("  Record for uuid", uuid, "removed.")
+      CommandResult(command, {status = "ok", output = "UUID removed."})
     elseif _type == "list" then
-      for k, v in pairs(tClearances) do
-        print(" ", k, v)
+      local lines = {}
+      for uuid, level in pairs(tClearances) do
+        lines[#lines + 1] = string.format("%s: %d", uuid, level)
       end
+      CommandPrint(lines)
+      CommandResult(command, {status = "console"})
+    else
+      CommandResult(command, {status = "no-subcommand", subcommand = _type})
     end
   end
-  function actions.help(command)
-    if not command or command == "help" then
-      print("  Usage: help <commandname>")
+  function actions.help(com)
+    if not com or com == "help" then
+      CommandResult(command, {status = "ok", output = "Usage: help <command>"})
       return
-    elseif command == "record" then
-      print("  Usage: record add <uuid:string> <level:string>")
-      print("  Usage: record update <uuid:string> <level:string>")
-      print("  Usage: record remove <uuid:string>")
-      print("  Usage: record list")
+    elseif com == "record" then
+      CommandPrint {
+        "Usage: record add <uuid:string> <level:string>",
+        "Usage: record update <uuid:string> <level:string>",
+        "Usage: record remove <uuid:string>",
+        "Usage: record list"
+      }
+      CommandResult(command, {status = "console"})
+      return
+    elseif com == "lockdown" then
+      CommandPrint {
+        "Usage: lockdown status",
+        "Usage: lockdown activate",
+        "Usage: lockdown deactivate"
+      }
       return
     end
-    print("  No help records for", command)
+    CommandResult(command, {status = "error", error = string.format("No help records for %s.", com)})
   end
   function actions.commands()
-    print("  Command, one of the following:")
+    local lines = {}
     for k in pairs(actions) do
-      print("   ", k)
+      lines[#lines + 1] = k
     end
+    CommandPrint(lines)
+    CommandResult(command, {status = "console"})
   end
   function actions.stop()
+    CommandResult(command, {status = "error", error = "Halted."})
     error("Halted.", 0)
   end
   actions.halt = actions.stop
@@ -147,13 +274,14 @@ local function Action(command)
   if actions[chunks[1]] then
     actions[chunks[1]](table.unpack(chunks, 2, #chunks))
   else
-    print("  No command", chunks[1])
+    CommandResult(command, {status = "error", error = "Unknown Command"})
   end
 end
 
 local function Main()
   DefineSettings()
   tClearances = LoadRecords()
+  redrawScreen()
   parallel.waitForAny(
     function() -- Transmission
       local Channel = settings.get("server.channel")
@@ -189,8 +317,26 @@ local function Main()
         userWindow.write("> ")
         term.setCursorPos(3, my)
         local command = read()
-        printWindow.redraw()
         Action(command)
+        redrawScreen()
+      end
+    end,
+    function() -- lockdown transmitter
+      local modem = peripheral.find("modem")
+      while true do
+        while bLockDown do
+          modem.transmit(nLockdownChannel, nLockdownChannel, {
+            type = "LOCKDOWN",
+            status = true,
+            bypass = minLockdownLevel
+          })
+          os.sleep(2)
+        end
+        modem.transmit(nLockdownChannel, nLockdownChannel, {
+          type = "LOCKDOWN",
+          status = false
+        })
+        os.sleep(2)
       end
     end
   )
